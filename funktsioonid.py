6acaab79a2b2f99e1d1f41186f8544e936f82fbf
kenk-k <kenk.kaur@gmail.com>
2025-11-12 16:25:13 +0200

    alpha versioon

---
+++ funktsioonid.py
@@ -0,0 +1,104 @@
+"""
+Autorid: Kaur Kenk, Nansen Palo
+"""
+
+import random
+from math import log
+
+def lineaar():
+
+  """Genereerib täisarvulise vastuse ja
+     sellele vastava lineaarvõrrandi."""
+  lahendus = random.randint(-20, 21)
+  a = random.choice([i for i in range(-20, 21) if i != 0])
+  b = random.randint(-20, 21)
+  c = a*lahendus + b
+  vorrand = f'${a}x + {b} = {c}$'
+  return vorrand, lahendus
+
+def ruut():
+
+  """Genereerib 2 täisarvulist vastust ja
+     nendele vastava ruutvõrrandi latexis"""
+  lahendus1 = random.randint(-20, 21)
+  lahendus2 = random.randint(-20, 21)
+  vorrandi_kordaja = random.choice([i for i in range(-4,5) if i != 0])
+  #TODO: teha nii, et vabaliige saab olla ka paremal pool nii, et
+  #numbrid oleksid täisarvud
+  #vabaliikmete_suhe = random.randint(-4, 5)
+  #while vabaliikmete_suhe == 0:
+  #  vabaliikmete_suhe = random.randint(-4, 5)
+  #vabaliige1 = (lahendus1*lahendus2*(1-(1/vabaliikmete_suhe)))
+  vabaliige1 = lahendus1*lahendus2
+  #vabaliige2 = (-lahendus1*lahendus2/vabaliikmete_suhe)
+  vabaliige2 = 0
+  lineaar_kordaja = -(lahendus1 + lahendus2)
+  if (vorrandi_kordaja*lineaar_kordaja < 0 and
+      vorrandi_kordaja*vabaliige1 < 0):
+    vorrand = ('$' + str(vorrandi_kordaja) + 'x^{2}'
+               + str(vorrandi_kordaja*lineaar_kordaja) + 'x' 
+               + str(vorrandi_kordaja*vabaliige1) + ' ='
+               + str(vorrandi_kordaja*vabaliige2) +'$')
+  elif vorrandi_kordaja*lineaar_kordaja < 0:
+    vorrand = ('$' + str(vorrandi_kordaja) + 'x^{2}'
+               + str(vorrandi_kordaja*lineaar_kordaja) + 'x +' 
+               + str(vorrandi_kordaja*vabaliige1) + ' ='
+               + str(vorrandi_kordaja*vabaliige2) +'$')
+  elif vorrandi_kordaja*vabaliige1 < 0:
+    vorrand = ('$' + str(vorrandi_kordaja) + 'x^{2} + '
+               + str(vorrandi_kordaja*lineaar_kordaja) + 'x' 
+               + str(vorrandi_kordaja*vabaliige1) + ' ='
+               + str(vorrandi_kordaja*vabaliige2) +'$')
+  else:
+    vorrand = ('$' + str(vorrandi_kordaja) + 'x^{2} + '
+               + str(vorrandi_kordaja*lineaar_kordaja) + 'x + ' 
+               + str(vorrandi_kordaja*vabaliige1) + ' ='
+               + str(vorrandi_kordaja*vabaliige2) +'$')
+
+  if vorrandi_kordaja == 1:
+    vorrand = vorrand[0:1] + vorrand[2:]
+  elif vorrandi_kordaja == -1:
+    vorrand = vorrand[0:2] + vorrand[3:]
+  return vorrand, lahendus1, lahendus2
+
+def eksponentsiaal():
+
+  """Genereerib latexis eksponentsiaalvõrrandi sõne ja
+     vastava täisarvulise vastuse"""
+  baas = random.choice([i for i in range(2, 4)])
+  
+  kordaja = random.choice([i for i in range(-5, 6) if i != 0])
+  
+
+  parem_pool = baas**random.randint(1,5)
+  lahendus = log(parem_pool, baas)
+  vorrand = f'${kordaja} \\cdot {baas}^{'x'} = {parem_pool*kordaja}$'
+  return vorrand, lahendus
+
+def logaritm():
+
+  """Genereerib latexi formaadis logaritmvõrrandi sõne ja 
+     vastava täisarvulise vastuse"""
+  baas = random.choice([2,3,5,10])
+  #valib suvaliselt kordaja -5st 5ni nii, et kordaja ei oleks 0
+  kordaja = random.choice([i for i in range(-5, 6) if i != 0])
+  #lahendus on baas astmes suvaline täisarv, täisarvuliste baaside
+  #korral on see täisarv.
+  #TODO: mõelda välja, kuidas saada tööle murdude korral
+  lahendus = baas**random.randint(1,4)
+  parem_pool = round(kordaja*log(lahendus, baas))
+  vorrand = ('$'+ str(kordaja)+ '\\log_{'
+             + str(baas) + '}x = ' + str(parem_pool) + '$')
+  return vorrand, lahendus
+
+#TODO: lisada ln-ile funktsioon
+
+# Trigonomeetriline funktsioon, ei tööta
+def trigonomeetriline():
+  x = sp.Symbol('x')
+  lahendus = random.randint(0, 361)
+  solution_rad = sp.rad(lahendus)
+  trig = random.choice([sp.sin, sp.cos])
+  c = trig_fun(solution_rad)
+  vorrand = sp.Eq(trig_fun(x), c)
+  return vorrand, lahendus, trig_fun
---
+++ main_oop.py
@@ -0,0 +1,306 @@
+"""
+Autorid: Kaur Kenk, Nansen Palo
+
+Teema: Koolimatemaatika mäng
+
+Vajalik programmi tööks: matplotlib, pythoni versioon vähemalt 3.10
+
+Inspiratsioon: kergelt inspireeritud pranglimisest.
+latexi displaymisega aitas: 
+https://www.tutorialspoint.com/how-to-display-latex-
+in-real-time-in-a-text-box-in-tkinter
+ja matplotlibi dokumentatsioon.
+tkinteriga aitas tkdocs.com ja eelnev kogemus keskkoolist, samuti oopiga
+aitas eelnev kogemus.
+
+"""
+import tkinter as tk
+from tkinter import ttk
+
+from random import randint
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+import funktsioonid as fun
+
+
+def main():
+
+
+    matplotlib.use('TkAgg')
+    prog = Programm()
+    prog.mainloop()
+
+
+class Programm(tk.Tk):
+
+
+    def __init__(self):
+
+        """Tekitab uue tkinteri akna, sätib paika õige akna suuruse
+        ning sätib kysimuse_counter ja oiged muutujad paika.
+        """
+        #Teeb läbi tk.Tk init funktsiooni
+        super().__init__()
+        self.kysimuse_counter = 1
+        self.oiged = 0
+
+        self.title('Matemaatika mäng')
+        #Võtab ekraani suuruse arvesse, et mängu aken oleks sama suur
+        #nii madala kui kõrge resolutsiooniga monitoridel.
+        self.ekraani_laius = self.winfo_screenwidth()
+        self.ekraani_korgus = self.winfo_screenheight()
+        self.geometry(f'{round(self.ekraani_laius/3.2)}x'
+                      + f'{round(self.ekraani_korgus/2.6)}')
+        #Raamid suurendavad end automaatselt akent suurendades
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.tiitel()
+        
+    def tiitel(self):
+
+        """Teeb tiitli raami ja selle sees olevad aknad."""
+        self.tiitel_raam = ttk.Frame(self)
+        self.tiitel_raam.rowconfigure(0, weight=2)
+        self.tiitel_raam.rowconfigure(1, weight=1)
+        self.tiitel_raam.columnconfigure(1, weight=1)
+        self.tiitel_raam.columnconfigure(2, weight=1)
+        self.tiitel_raam.columnconfigure(3, weight=1)
+        self.tiitel_raam.grid(column = 0, row = 0,
+                              sticky = 'nsew')
+
+        self.tiitel_nimi = ttk.Label(self.tiitel_raam,
+                                     anchor='center',
+                                     font=('Arial', 40),
+                                     text = 'Matemaatika mäng')
+        self.tiitel_nimi.grid(column = 1, row = 0, columnspan=3,
+                              sticky = 'nsew')
+
+
+        self.tiitelnupp_stiil = ttk.Style()
+        self.tiitelnupp_stiil.configure('Tiitel.TButton',font=('Arial',25))
+        self.start_nupp = ttk.Button(self.tiitel_raam, text='Mängima!',
+                                     style='Tiitel.TButton',
+                                     command=self.mang)
+        self.start_nupp.grid(column = 1, row = 1, sticky = 'n')
+
+        self.juhised_nupp = ttk.Button(self.tiitel_raam,
+                                       text = 'Juhised',
+                                       style='Tiitel.TButton',
+                                       command=self.juhised)
+        self.juhised_nupp.grid(column = 2, row = 1, sticky = 'n')
+        
+        self.kinni_nupp = ttk.Button(self.tiitel_raam,
+                                     text='Sulge',
+                                     style='Tiitel.TButton',
+                                     command=self.destroy)
+        self.kinni_nupp.grid(column=3, row=1, sticky = 'n')
+
+    def juhised(self):
+
+        """Teeb uue toplevel akna, kus on juhised.txt failist võetud
+        sisestuse juhised.
+        """
+        
+        self.juhised = tk.Toplevel(self)
+        #akna suurendamisel suureneb juhiste_raam
+        self.juhised.rowconfigure(0, weight = 1)
+        self.juhised.columnconfigure(0, weight = 1)
+        self.juhised.title('Sisestuse juhised')
+        #vt. init
+        self.juhised.geometry(f'{round(self.ekraani_laius/2)}x'
+                              + f'{round(self.ekraani_korgus/2)}')
+
+        self.juhiste_raam = ttk.Frame(self.juhised)
+        self.juhiste_raam.grid(column = 0, row = 0,
+                               sticky = 'nsew')
+        #Teksti rida on 3 korda suurem kui tagasinupu rida                       
+        self.juhiste_raam.rowconfigure(0, weight = 3)
+        self.juhiste_raam.rowconfigure(1, weight = 1)
+
+        self.juhiste_raam.columnconfigure(0, weight=1)
+
+        self.juhised_tiitel = ttk.Label(self.juhiste_raam,
+                                        text = 'Sisestuse juhised',
+                                        width = 50, font = 44)
+        self.juhised_tiitel.grid(column = 0, row = 0)
+
+        #Juhiste tekst tuleb failist juhised.txt, kuna siia kirjutamine
+        #võtab liiga palju ruumi
+        self.juhisetekstifail = open('juhised.txt', encoding='utf-8')
+        self.juhised_text = ttk.Label(self.juhiste_raam, justify='center',
+                                      text = self.juhisetekstifail.read(),
+                                      font=('Arial', 16))
+        self.juhised_text.grid(column = 0, row = 0, sticky= 'ns')
+        self.juhisetekstifail.close()
+
+        self.juhiste_nupp_stiil = ttk.Style()
+        self.juhiste_nupp_stiil.configure('Juhised.TButton',
+                                          font=('Arial', 20)) 
+        self.juhised_kinni_nupp = ttk.Button(self.juhiste_raam, text = 'Sulge',
+                                             style='Juhised.TButton',
+                                             command = self.juhised.destroy)
+        self.juhised_kinni_nupp.grid(column = 0, row = 1)
+        
+    def mang(self):
+
+        """Teeb raami, kus sees küsitlus toimub
+        ja selle raami sees olevad aknad."""
+        self.tiitel_raam.destroy()
+
+        self.mangu_raam = ttk.Frame(self)
+        self.mangu_raam.grid(column = 0, row = 0,
+                             sticky = 'nsew')
+        self.mangu_raam.columnconfigure(0, weight=1)
+        self.mangu_raam.rowconfigure(0, weight = 2)
+        self.mangu_raam.rowconfigure(1, weight = 1)
+        self.mangu_raam.rowconfigure(2, weight = 1)
+        self.mangu_raam.rowconfigure(3, weight = 1)
+        self.mangu_raam.rowconfigure(4, weight = 1)
+        self.uus_funktsioon()
+
+        self.funktsioon = ttk.Label(self.mangu_raam)
+        self.funktsioon.grid(column = 0, row = 0, sticky= 'ns')
+
+        self.figuur = matplotlib.figure.Figure(figsize = (10, 3), dpi = 100)
+        self.latex = FigureCanvasTkAgg(self.figuur, master = self.funktsioon)
+        self.latex.get_tk_widget().grid(column = 0, row = 0, sticky='ns')
+    
+        self.figuur.text(0.5, 0.5, self.vorrand,
+                         horizontalalignment = 'center',
+                         verticalalignment = 'center', fontsize = 40)
+        self.latex.draw()
+
+        self.sisestus = tk.StringVar()
+        self.vastuse_kast = ttk.Entry(self.mangu_raam,
+                                      font = ('Arial', 20),
+                                      textvariable = self.sisestus)
+        self.vastuse_kast.grid(column = 0, row = 1, sticky = 'ns')
+
+        self.vastamis_nupp = ttk.Button(self.mangu_raam, text = 'Kontrolli',
+                                        command = self.kontrolli)
+        self.vastamis_nupp.grid(column = 0, row = 2)
+
+        self.loendur = ttk.Label(self.mangu_raam,
+                                 text = f'{self.kysimuse_counter}/20')
+        self.loendur.grid(column = 0, row = 3)
+
+        self.sisestuse_info = ttk.Label(self.mangu_raam)
+        self.sisestuse_info.grid(column = 0, row = 4)
+
+        self.bind('<Return>', self.kontrolli)
+
+    def uus_funktsioon(self):
+        """valib suvaliselt ühte tüüpi võrrandi ja
+           salvestab võrrandi latexi sõne ja vastused
+           mängu jaoks muutujatesse"""
+        #TODO: rohkem võrrandeid mängu
+        self.vorrandi_number = randint(1,4)
+        #self.vorrandi_number = 2
+        match self.vorrandi_number:
+            case 1:
+                self.vorrand, self.lahendus = fun.lineaar()
+            case 2:
+                self.vorrand, self.lahendus1, self.lahendus2 = fun.ruut()
+            case 3:
+                self.vorrand, self.lahendus = fun.eksponentsiaal()
+            case 4:
+                self.vorrand, self.lahendus = fun.logaritm()
+    
+    def kontrolli(self, event=None):
+        """Kontrollib vastust, mis kasutaja sisestas ja 
+           uuendab mängu raamil väärtusi. Kui on vastatud
+           piisavatele küsimustele, lõpetab mängu"""
+
+        self.vastus = self.sisestus.get()
+        #vaatab, kas võrrand on ruutfunktsioon,
+        #sest sellel on kaks vastust
+        if self.vorrandi_number == 2:
+            #teeb vastused listiks, vaatab, kas vastused on sees
+            #või mitte ja siis kustutab listist.
+            vastused = self.vastus.split(', ')
+            if (str(self.lahendus1) in self.vastus and
+                str(self.lahendus2) in vastused):
+                vastused.pop(vastused.index(str(self.lahendus1)))
+                vastused.pop(vastused.index(str(self.lahendus2)))
+                #kui oli täpselt kaks vastust ja need olid õiged,
+                #tuleb õige vastus
+                if vastused == []:
+                    self.oiged += 1
+                    self.sisestuse_info.configure(text='Õige vastus!')
+                else:
+                    self.sisestuse_info.configure(text='Vale vastus!')
+            else:
+                self.sisestuse_info.configure(text='Vale vastus!')
+        else:
+            try:
+                self.vastuse_number = int(self.vastus)
+            except ValueError:
+                self.sisestuse_info.configure(text='Sisesta vastus täisarvuna')
+                return None
+            if int(self.vastus) == self.lahendus:
+                self.oiged += 1
+                self.sisestuse_info.configure(text='Õige vastus!')
+            else:
+                self.sisestuse_info.configure(text='Vale vastus!')
+
+        self.uus_funktsioon()
+
+        self.kysimuse_counter += 1
+        #Lõpetab mängu, kui 20 küsimust on vastatud
+        if self.kysimuse_counter == 20:
+            self.lopp()
+        else:
+            #Puhastab figuuri ja kirjutab uue võrrandi figuurile
+            self.figuur.clear()
+            self.figuur.text(0.5, 0.5, self.vorrand,
+                             horizontalalignment = 'center',
+                             verticalalignment = 'center',
+                             fontsize = 30)
+            self.latex.draw()
+            #uuendab loendurit
+            self.loendur.configure(text = f'{self.kysimuse_counter}/20')
+            self.vastuse_kast.delete(0, 'end')
+
+    def lopp(self):
+
+        """Teeb lõpuekraani raami ja näitab tulemust.
+        Saab minna tiitellehele"""
+        #TODO: teha lõpuekraan valmis
+        #Teeb nii, et Enterit vajutades enam ei kutsutaks kontrolli()
+        #funktsiooni
+        self.unbind('<Return>')
+
+        self.mangu_raam.destroy()
+        self.lopp_raam = ttk.Frame(self)
+        self.lopp_raam.grid(column = 0, row = 0, sticky = 'nsew')
+        self.lopp_raam.columnconfigure(0, weight = 1)
+        self.lopp_raam.columnconfigure(1, weight = 1)
+        self.lopp_raam.rowconfigure(0, weight = 3)
+        self.lopp_raam.rowconfigure(1, weight = 1)
+        self.tulemus = ttk.Label(self.lopp_raam,
+                                 text = f'Sinu tulemus on {self.oiged}/20.',
+                                 font = ('Arial', 30))
+        self.tulemus.grid(column = 0, row = 0, columnspan = 2)
+        self.uuesti_nupp = ttk.Button(self.lopp_raam,
+                                      text = 'Tagasi tiitelehele',
+                                      command = self.uuesti)
+        self.uuesti_nupp.grid(column = 0, row = 1)
+        self.sulgemis_nupp_lopp = ttk.Button(self.lopp_raam,
+                                             text = 'Sulge',
+                                             command = self.destroy)
+        self.sulgemis_nupp_lopp.grid(column = 1, row = 1)
+
+    def uuesti(self):
+
+        """Taastab muutujate kysimuse_counter ja oiged algsed väärtused
+        ning läheb tagasi tiitellehele."""
+        self.kysimuse_counter = 1
+        self.oiged = 0
+        self.lopp_raam.destroy()
+        self.tiitel()
+        
+if __name__ == '__main__':
+    main()
